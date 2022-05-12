1c95375ee89bad727c2d4b46ea56346d03d1d287
Cristian <kazragore96@gmail.com>
2022-05-11 19:56:45 -0500

    feat: Agregado permisos necesarios para descargar y escribir un archivo.

--- App.tsx
+++ App.tsx
@@ -1,6 +1,34 @@
-import React, {useState} from 'react';
-import {Button, StyleSheet, Text, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {
+  Button,
+  PermissionsAndroid,
+  ScrollView,
+  StyleSheet,
+  Text,
+} from 'react-native';
 import * as fs from 'react-native-fs';
+
+const checkRead = async () => {
+  const PERMISSION = PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE;
+  const permiso = await PermissionsAndroid.check(PERMISSION);
+
+  if (!permiso) {
+    await PermissionsAndroid.request(PERMISSION);
+  }
+
+  return permiso;
+};
+
+const checkWrite = async () => {
+  const PERMISSION = PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE;
+  const permiso = await PermissionsAndroid.check(PERMISSION);
+
+  if (!permiso) {
+    await PermissionsAndroid.request(PERMISSION);
+  }
+
+  return permiso;
+};
 
 const downloadFile = async (url: string, fileName: string) =>
   new Promise(async (resolve, reject) => {
@@ -18,26 +46,66 @@ const downloadFile = async (url: string, fileName: string) =>
   });
 
 const App = () => {
+  const [dirs, setDirs] = useState(null);
+  const [status, setStatus] = useState(0);
+  const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
+  const [permisos, setPermisos] = useState({
+    read: false,
+    write: false,
+  });
+
+  const checkPermissions = async () => {
+    const read = await checkRead();
+    const write = await checkWrite();
+
+    setPermisos({read, write});
+  };
 
   const handleDownloadFile = async () => {
     setLoading(true);
     const url = 'http://samples.leanpub.com/thereactnativebook-sample.pdf';
-    const res = await downloadFile(url, 'sample2.pdf');
+    const res = await downloadFile(url, 'sample2.pdf').catch(err =>
+      setError(err),
+    );
     console.log({res});
+    setStatus((res as any).statusCode);
     setLoading(false);
   };
 
+  useEffect(() => {
+    checkPermissions();
+  }, []);
+
+  useEffect(() => {
+    fs.readDir(fs.DownloadDirectoryPath)
+      .then(data => {
+        setDirs(data as any);
+      })
+      .catch(err => setError(err));
+  }, []);
+
+  useEffect(() => {
+    fs.getAllExternalFilesDirs().then(data =>
+      console.log(JSON.stringify({data}, null, 4)),
+    );
+  }, []);
+
   return (
-    <View style={styles.center}>
+    <ScrollView style={styles.center}>
+      <Text> READ: {permisos.read ? 'CON ACCESOS' : 'SIN ACCESOS'} </Text>
+      <Text> WRITE: {permisos.write ? 'CON ACCESOS' : 'SIN ACCESOS'} </Text>
+      <Text> STATUS: {status}</Text>
       <Button title="Descargar" onPress={handleDownloadFile} />
       {loading && <Text>Descargando...</Text>}
-    </View>
+      <Text> Dir: {JSON.stringify(dirs, null, 4)}</Text>
+      <Text> Error: {JSON.stringify(error, null, 4)}</Text>
+    </ScrollView>
   );
 };
 
 export default App;
 
 const styles = StyleSheet.create({
-  center: {flex: 1, justifyContent: 'center', alignItems: 'center'},
+  center: {flex: 1},
 });
